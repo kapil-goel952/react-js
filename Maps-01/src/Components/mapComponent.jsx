53744d22c2ebd42b435947ad9327eb7dcf61f227
kapil-goel952 <goelk849@gmail.com>
2026-05-21 23:53:09 +0530

    updated the code and UI

--- Maps-01/src/Components/mapComponent.jsx
+++ Maps-01/src/Components/mapComponent.jsx
@@ -3,26 +3,41 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const MapComponent = () => {
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div className='h-screen w-full bg-black p-4'>
 
-      <MapContainer
-        center={[28.6139, 77.2090]}
-        zoom={13}
-        style={{ height: "100%", width: "100%" }}
-      >
+      <div className='h-full w-full overflow-hidden rounded-4xl shadow-2xl border border-gray-800'>
 
-        <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
+        <MapContainer
+          center={[30.7333, 76.7794]}
+          zoom={13}
+          scrollWheelZoom={true}
+          className='h-full w-full z-0'
+        >
 
-        <Marker position={[28.6139, 77.2090]}>
-          <Popup>
-            Delhi 🚀
-          </Popup>
-        </Marker>
+          {/* Satellite / Aerial View */}
+          <TileLayer
+            attribution='Tiles &copy; Esri'
+            url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+          />
 
-      </MapContainer>
+          {/* Marker */}
+          <Marker position={[30.7333, 76.7794]}>
+
+            <Popup>
+              <div className='font-semibold text-lg'>
+                Chandigarh 🚀
+              </div>
+
+              <p className='text-sm'>
+                Satellite view using Leaflet
+              </p>
+            </Popup>
+
+          </Marker>
+
+        </MapContainer>
+
+      </div>
 
     </div>
   )
