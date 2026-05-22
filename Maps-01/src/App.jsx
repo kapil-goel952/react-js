582a7f98f4b5b2413e8674ed3c6d564fca35647a
kapil-goel952 <goelk849@gmail.com>
2026-05-22 23:52:31 +0530

    updated the Structure ans the styling

--- Maps-01/src/App.jsx
+++ Maps-01/src/App.jsx
@@ -1,20 +1,18 @@
 import './index.css'
+
 import MapComponent from './Components/mapComponent'
 
-function App() {
 
+function App() {
+ 
   return (
     <div style={{
-      display: "flex",
-      height: "100%",
-      width: "100%",
-      alignItems: "center",
-      justifyContent: "center"
+      flex:1,
+      height:{"100%"},
+      width:100,
     }}>
-
-      <MapComponent />
-
-    </div>
+     <MapComponent />    
+    </div >
   )
 }
 
--- Maps-01/src/Components/mapComponent.jsx
+++ Maps-01/src/Components/mapComponent.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-
 const MapComponent = () => {
   return (
-    <div>
+    <div  style={{
+          height: '200px',
+          width: '400px',
+          borderRadius: '8',
+        }}>
 
       <MapContainer
         center={[30.7333, 76.7794]}
         zoom={13}
         scrollWheelZoom={true}
         style={{
-          height: '80vh',
-          width: '80vw',
+          height: '200px',
+          width: '400px',
           borderRadius: '8',
         }}
       >
@@ -22,7 +25,13 @@ const MapComponent = () => {
           url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
         />
 
-      
+        <Marker position={[30.7333, 76.7794]}>
+
+          <Popup>
+            Chandigarh 🚀
+          </Popup>
+
+        </Marker>
 
       </MapContainer>
 
