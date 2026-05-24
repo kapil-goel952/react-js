503650afb58f38e15bb71d7cef23e57d5579dbf0
kapil-goel952 <goelk849@gmail.com>
2026-05-24 23:31:41 +0530

    asdfF

--- Maps-01/src/Components/Navbar.jsx
+++ Maps-01/src/Components/Navbar.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>   
-        <div>
-          <img src="https://images.unsplash.com/photo-1603189617530-6d32306f57c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fEJNV3xlbnwwfHwwfHx8MA%3D%3D" alt="logo"  style={
-          {
-            height:"20px",
-            width:"20px",
-            borderRadius:"20%"
-          }
-          } />
-        </div>
+     
       
     </div>
   )
